930a3c57189ffe726e9fcf28669bf1d3a55d5276
toseph-here <ansaritosif619@gmail.com>
2025-07-13 20:10:56 +0530

    Create player.js

---
+++ player.js
@@ -0,0 +1,38 @@
+let player = {
+  x: 1,
+  y: 1,
+  color: 'red',
+  size: tileSize * 0.7,
+  speed: 1
+};
+
+document.addEventListener('keydown', (e) => {
+  let newX = player.x;
+  let newY = player.y;
+
+  if (e.key === 'ArrowUp') newY--;
+  else if (e.key === 'ArrowDown') newY++;
+  else if (e.key === 'ArrowLeft') newX--;
+  else if (e.key === 'ArrowRight') newX++;
+
+  if (maze[newY][newX] === 0) {
+    player.x = newX;
+    player.y = newY;
+    drawEverything();
+  }
+});
+
+function drawPlayer() {
+  const px = player.x * tileSize + tileSize / 2;
+  const py = player.y * tileSize + tileSize / 2;
+
+  ctx.fillStyle = player.color;
+  ctx.beginPath();
+  ctx.arc(px, py, player.size / 2, 0, Math.PI * 2);
+  ctx.fill();
+}
+
+function drawEverything() {
+  drawMaze();
+  drawPlayer();
+}
